ffc43454e8801671d7852ba09851d186e9b7e002
kimporzang@gamil.com <kimporzang@gmail.com>
2022-11-19 00:16:38 +0900

    for문으로 버튼클릭동작 최소화

--- Tap-Button/main.js
+++ Tap-Button/main.js
@@ -2,15 +2,15 @@
 const tabButtons = document.querySelectorAll(".tab-list__button");
 const tabContents = document.querySelectorAll(".tab-list__content");
 
-//첫번째 버튼 클릭 시 동작
-tabButtons[0].addEventListener('click', () => {
-    //기존 버튼 효과 제거
-    removeClass()   // 함수명은 동사+명사 형태로 작성한다.
-    
-    //클릭된 버튼에 효과 추가
-    tabButtons[0].classList.add('click-add');
-    tabContents[0].classList.add('show');
-});
+//(n번째)버튼 클릭 시 동작
+for (let i=0; i<3; i++){   //반복문을 통해 버튼 클릭 동작을 최소화한다.
+    tabButtons[i].addEventListener('click', () => {
+        removeClass();
+        tabButtons[i].classList.add('click-add');
+        tabContents[i].classList.add('show');
+    });
+
+}
 
 function removeClass(){       // 클래스를 제거하는 기능인 함수를 만든다.
     for (let i=0; i<3; i++){
