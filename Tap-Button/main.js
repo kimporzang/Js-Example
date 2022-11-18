6f07f47485538ee8c26d142a6a2c972c03a56c84
kimporzang@gamil.com <kimporzang@gmail.com>
2022-11-19 00:14:06 +0900

    removeClass라는 함수생성

--- Tap-Button/main.js
+++ Tap-Button/main.js
@@ -5,11 +5,18 @@ const tabContents = document.querySelectorAll(".tab-list__content");
 //첫번째 버튼 클릭 시 동작
 tabButtons[0].addEventListener('click', () => {
     //기존 버튼 효과 제거
-    tabButtons[1].classList.remove('click-add');
-    tabButtons[2].classList.remove('click-add');
-    tabContents[1].classList.remove('show');
-    tabContents[2].classList.remove('show');
+    removeClass()   // 함수명은 동사+명사 형태로 작성한다.
+    
     //클릭된 버튼에 효과 추가
     tabButtons[0].classList.add('click-add');
     tabContents[0].classList.add('show');
 });
+
+function removeClass(){       // 클래스를 제거하는 기능인 함수를 만든다.
+    for (let i=0; i<3; i++){
+        tabButtons[i].classList.remove('click-add');
+        tabContents[i].classList.remove('show');
+    }
+}
+
+
