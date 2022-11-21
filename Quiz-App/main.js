1263a4ae398f006d9fefc16b173b58e44e0e7ee5
kimporzang@gamil.com <kimporzang@gmail.com>
2022-11-21 16:30:30 +0900

    변수 선언

---
+++ Quiz-App/main.js
@@ -0,0 +1,6 @@
+// 먼저 사용할 값을 변수에 담아줍니다.
+const answerBoxs= document.querySelectorAll(".quiz-box__answerbox")
+const numberBox = document.querySelectorAll(".number-box")
+const checkIcon = document.querySelectorAll("#check-icon")
+const submitButton = document.querySelector(".quiz-box__button")
+let myAnswer = null   
